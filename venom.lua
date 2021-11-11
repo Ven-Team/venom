6df9a9a12cad26819784c7fd1a588436e92716a1
Ven-Team <92828388+Ven-Team@users.noreply.github.com>
2021-11-12 01:46:35 +0300

    Add files via upload

--- venom.lua
+++ venom.lua
@@ -10831,7 +10831,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/Ven-Team/venomFiles/master/LaricAFiles"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/Ven-Team/venomFiles/master/LaricAFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n⌔︙الملف -› *"..FileName.."*\n⌔︙تم تعطيله وحذفه من البوت بنجاح") 
